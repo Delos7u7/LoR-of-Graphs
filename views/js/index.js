bcb91ffc5da877b72f3bcfa744aa6d25cfbfd0f7
Jose Ramon <esedelos@hotmail.com>
2023-11-08 00:28:38 -0600

    Diseño de historial en rpoceso

--- views/js/index.js
+++ views/js/index.js
@@ -1,52 +1,53 @@
-const apiKey = "RGAPI-9d80bf01-e506-48ae-bbaf-eac2195feb98";
+const apiKey="RGAPI-fd028d4e-0174-41c5-905b-acecb9317b70";
 let datapuuid;
-let region = "";
-let locationn = "";
-let matchId = "";
-document.getElementById('search').addEventListener('submit', function (event) {
+let region="";
+let locationn="";
+let matchId="";
+document.getElementById('search').addEventListener('submit', function(event){
     event.preventDefault();
     region = document.getElementById('region').value;
     const summonerName = document.getElementById('summoner').value;
-    let suummonerEndPoint = "https://" + region + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "/?api_key=" + apiKey;
+    let suummonerEndPoint = "https://"+region+".api.riotgames.com/lol/summoner/v4/summoners/by-name/"+summonerName+"/?api_key="+apiKey;
     if (region === 'la1' || region === 'la2' || region === 'na1') {
         locationn = "americas";
-    } else if (region === 'eun1' || region === 'euw1' || region === 'ru') {
+      } else if (region === 'eun1' || region === 'euw1' || region === 'ru') {
         locationn = "europe";
-    } else {
+      } else {
         locationn = "asia";
-    }
+      }
     fetch(suummonerEndPoint)
+    .then(response => response.json())
+    .then(data => {
+       // window.location.href = "/history";
+        datapuuid=data.puuid;
+        console.log("puuid "+datapuuid);
+        let matchEndPoint = "https://"+locationn+".api.riotgames.com/lor/match/v1/matches/by-puuid/"+datapuuid+"/ids?api_key="+apiKey;
+        fetch(matchEndPoint)
         .then(response => response.json())
         .then(data => {
-            // window.location.href = "/history";
-            datapuuid = data.puuid;
-            let matchEndPoint = "https://" + locationn + ".api.riotgames.com/lor/match/v1/matches/by-puuid/" + datapuuid + "/ids?api_key=" + apiKey;
-            fetch(matchEndPoint)
+            for (let i = 0; i < data.length-10; i++) {
+                setTimeout(() => {
+                  console.log("Match numero: "+i+" "+data[i]);
+                
+                matchId=data[i];
+                let matchIdEndPoint = "https://"+locationn+".api.riotgames.com/lor/match/v1/matches/"+matchId+"?api_key="+apiKey;
+               fetch(matchIdEndPoint)
                 .then(response => response.json())
                 .then(data => {
-                    for (let i = 0; i < data.length; i++) {
-                        setTimeout(() => {
-                            console.log("Match numero: " + i + " " + data[i]);
-
-                            matchId = data[i];
-                            let matchIdEndPoint = "https://" + locationn + ".api.riotgames.com/lor/match/v1/matches/" + matchId + "?api_key=" + apiKey;
-                            fetch(matchIdEndPoint)
-                                .then(response => response.json())
-                                .then(data => {
-                                    console.log("Info data" + data.info.players[0].deck_code + " w/l player1" + data.info.players[0].game_outcome + " total de turnos: " + data.info.total_turn_count);
-                                    console.log("Info data 2" + data.info.players[1].deck_code + " w/l player 2" + data.info.players[1].game_outcome + " total de turnos: " + data.info.total_turn_count);
-                                })
-                        }, i * 1000); // Se agrega un retraso de 0.5 segundos (500 ms) por cada iteración
-                    }
-
+                  console.log("Info data"+data.info.players[0].deck_code+" w/l player1"+data.info.players[0].game_outcome+" total de turnos: " +data.info.total_turn_count);
+                    console.log("Info data 2"+data.info.players[1].deck_code+" w/l player 2"+data.info.players[1].game_outcome+" total de turnos: " +data.info.total_turn_count);
                 })
-                .catch(error => {
-                    console.error("Error en la solicitud de la API match");
-                })
+            }, i * 1000); // Se agrega un retraso de 0.5 segundos (500 ms) por cada iteración
+              }
+
         })
-        .catch(error => {
-            console.error("Error en la solicitud de la API");
+        .catch(error =>{
+            console.error("Error en la solicitud de la API match");
         })
+    })
+    .catch(error =>{
+        console.error("Error en la solicitud de la API");
+    })
 
 })
 
